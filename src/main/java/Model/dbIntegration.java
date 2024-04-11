1a95429e59ef7bcdae1a33c25bc0f099a6086ac4
“bzhoupsu” <zhoupeiqiao@gmail.com>
2024-04-10 20:48:35 -0400

    Connect to mysql

---
+++ src/main/java/Model/dbIntegration.java
@@ -0,0 +1,37 @@
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
+public class dbIntegration {
+    public static void main(String[] args) {
+        Connection connect = null;
+        Statement statement = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/newsPublish",
+                    "newuser", "testing");
+
+            // Statements allow to issue SQL queries to the database
+            statement = connect.createStatement();
+            // Result set get the result of the SQL query
+            resultSet = statement.executeQuery("select * from newsPublish.person");
+            while (resultSet.next()) {
+                String login = resultSet.getString("LoginID");
+                String pwd = resultSet.getString("Password");
+                System.out.println("LoginID: " + login);
+                System.out.println("Password: " + pwd);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
